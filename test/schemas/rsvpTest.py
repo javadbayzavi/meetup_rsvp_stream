dfa39e8cffdf2e587a7bd0ac170c5c503d583e33
Javad Bayzavi <javadbayzavi@gmail.com>
2022-05-17 13:28:44 +0430

    testing phase done

--- test/schemas/rsvpTest.py
+++ test/schemas/rsvpTest.py
@@ -5,13 +5,25 @@ from schemas.meetup.rsvp import rsvp
 
 class TestRSVP(unittest.TestCase):
 
-    def test_model(self):
+    def test_rsvp(self):
         self.assertRaises(Exception, rsvp(True))
-        temp = rsvp(12)
-        self.assertEqual(temp.id , 12)
-        temp2 = rsvp()
-        self.assertLess(temp2.id , 10000)
-        self.assertGreater(temp2.id , 0)
+        temp = {
+            "group_name" : "group",
+            "group_city" : "London",
+            "group_country" : "uk",
+            "group_lon" : "12.0",
+            "group_lat" : "0.12",
+            "group_urlname" : "http://www.abc.org",
+            "group_state" : "lnd"
+        }
+        rsvp_tem = rsvp(12,"Visible","Test","2","1234567890",temp)
+        self.assertEqual(rsvp_tem.id , 12)
+        self.assertEqual(rsvp_tem.group_name , "London")
+        self.assertEqual(rsvp_tem.group_city , "uk")
+        self.assertEqual(rsvp_tem.group_lon , "12.0")
+        self.assertEqual(rsvp_tem.group_lat , "0.12")
+        self.assertEqual(rsvp_tem.urlname , "http://www.abc.org")
+        self.assertEqual(rsvp_tem.group_state , "lnd")
 
 
 #inject test class into unittest
